a9ef7961cc689c366fa894180839658af509b84b
CF Infrastructure <cf-infrastructure@pivotal.io>
2018-01-24 02:01:08 +0000

    Release az-automation 0.2.0

--- az-automation.rb
+++ az-automation.rb
@@ -5,10 +5,10 @@ class AzAutomation < Formula
 
   if OS.mac?
     url "https://github.com/genevievelesperance/az-automation/releases/download/#{version}/az-automation-#{version}-darwin-amd64"
-    sha256 ""
+    sha256 "ed9500e522cfb0c66bd84f8d496491913d6a161340af0a76884219645026f0de"
   elsif OS.linux?
     url "https://github.com/genevievelesperance/az-automation/releases/download/#{version}/az-automation-#{version}-linux-amd64"
-    sha256 ""
+    sha256 "318c392d355d1b903404c7788e028a736e859d5d351f40f1187499baa9557781"
   end
 
   depends_on :arch => :x86_64
