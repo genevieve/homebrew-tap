70908385f0899a8d29096112686c2eaeafd9e141
Genevieve LEsperance <glesperance@pivotal.io>
2018-01-29 15:54:50 -0800

    Use leftovers from library/cli.

--- leftovers.rb
+++ leftovers.rb
@@ -1,11 +1,11 @@
 class Leftovers < Formula
   desc "Command line utility for cleaning orphaned IAAS resources."
-  homepage "https://github.com/genevievelesperance/leftovers-cli"
-  version "v0.2.0"
+  homepage "https://github.com/genevievelesperance/leftovers"
+  version "v0.8.0"
 
   if OS.mac?
-    url "https://github.com/genevievelesperance/leftovers-cli/releases/download/#{version}/leftovers-cli-#{version}_osx"
-    sha256 "7244c2d5e292e450d8f8a0cd83c54d8b2406070e9bd220e78960e45fe700df2d"
+    url "https://github.com/genevievelesperance/leftovers/releases/download/#{version}/leftovers-#{version}_osx"
+    sha256 "85e8707b1bafa458ed029da64e95e5ae656f2cbe2a35e0c40713c873bd9f757d"
   end
 
   depends_on :arch => :x86_64
@@ -13,7 +13,7 @@ class Leftovers < Formula
   def install
     binary_name = "leftovers"
     if OS.mac?
-      bin.install "leftovers-cli-#{version}_osx" => binary_name
+      bin.install "leftovers-#{version}_osx" => binary_name
     end
   end
 
