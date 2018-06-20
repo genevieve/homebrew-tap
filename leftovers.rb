ee235d2eea36c4ec97f0f58c8987dfb150409f54
CF Infrastructure <cf-infrastructure@pivotal.io>
2018-06-20 15:33:58 +0000

    Release leftovers 0.32.0

--- leftovers.rb
+++ leftovers.rb
@@ -1,14 +1,14 @@
 class Leftovers < Formula
   desc "Command line utility for cleaning orphaned IAAS resources."
   homepage "https://github.com/genevieve/leftovers"
-  version "v0.31.0"
+  version "v0.32.0"
 
   if OS.mac?
     url "https://github.com/genevieve/leftovers/releases/download/#{version}/leftovers-#{version}-darwin-amd64"
-    sha256 "9cd642b12446920dbc3cd313066f9961b4ca60cd234115a7be9a55ce25c911b7"
+    sha256 "ad0ccb4c070f6e5244fa3fffed4f4ba0a49a600f1c6b5eb2bca366d9ee2873d0"
   elsif OS.linux?
     url "https://github.com/genevieve/leftovers/releases/download/#{version}/leftovers-#{version}-linux-amd64"
-    sha256 "a5bc31131e0774226084528f3461c40808211be2ccef2aa4014725869dbcb6db"
+    sha256 "2143a411e71e63da0bab8b1b16ab3f31407dc38b6f22a9cfa85c2cd04ac4b425"
   end
 
   depends_on :arch => :x86_64
